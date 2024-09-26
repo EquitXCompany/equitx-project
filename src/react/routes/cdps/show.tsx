73c79b1d802b9f14ff5d67de3ee4d5a100767197
Blaine Heffron <baheffron@gmail.com>
2024-09-26 16:39:59 -0400

    show proper decimals, use bignumber

--- src/react/routes/cdps/show.tsx
+++ src/react/routes/cdps/show.tsx
@@ -2,12 +2,13 @@ import { useLoaderData, useParams, Link } from "react-router-dom";
 import type { LoaderFunction } from "react-router-dom";
 import xasset from "../../../contracts/xasset";
 import type { CDP } from "xasset";
+import BigNumber from "bignumber.js";
 
 interface Data {
   cdp: CDP | undefined;
   decimals: number;
-  lastpriceXLM: number;
-  lastpriceAsset: number;
+  lastpriceXLM: BigNumber;
+  lastpriceAsset: BigNumber;
   symbolAsset: string;
 }
 
@@ -16,12 +17,8 @@ export const loader: LoaderFunction = async ({ params }): Promise<Data> => {
   return {
     cdp: await xasset.cdp({ lender }).then((tx) => tx.result),
     decimals: 7, // FIXME: get from xasset (to be implemented as part of ft)
-    lastpriceXLM:
-      Number(await xasset.lastprice_xlm().then((t) => t.result.price)) /
-      10 ** 14, // FIXME: get `14` from xasset (currently erroring in stellar-sdk)
-    lastpriceAsset:
-      Number(await xasset.lastprice_asset().then((t) => t.result.price)) /
-      10 ** 14, // FIXME: get `14` from xasset (currently erroring in stellar-sdk)
+    lastpriceXLM: new BigNumber(await xasset.lastprice_xlm().then((t) => t.result.price)).div(10 ** 14),
+    lastpriceAsset: new BigNumber(await xasset.lastprice_asset().then((t) => t.result.price)).div(10 ** 14),
     symbolAsset: "xUSD", // FIXME: get from xasset (to be implemented as part of ft)
   };
 };
@@ -30,6 +27,11 @@ function Show() {
   const { lender } = useParams() as { lender: string };
   const { cdp, decimals, lastpriceXLM, lastpriceAsset, symbolAsset } =
     useLoaderData() as Awaited<Data>;
+
+  const formatNumber = (value: BigNumber | number, decimalPlaces: number) => {
+    return new BigNumber(value).toFixed(decimalPlaces);
+  };
+
   return (
     <>
       <Link to="/">← Back to List</Link>
@@ -37,14 +39,14 @@ function Show() {
       {cdp && (
         <>
           <p>Status: {cdp.status.tag}</p>
-          <p>Ratio: {cdp.collateralization_ratio / 100}%</p>
+          <p>Ratio: {formatNumber(cdp.collateralization_ratio / 100, 2)}%</p>
           <p>
-            XLM Locked: {Number(cdp.xlm_deposited / 10n ** BigInt(decimals))}
+            XLM Locked: {formatNumber(new BigNumber(cdp.xlm_deposited.toString()).div(10 ** decimals), decimals)}
           </p>
-          <p>USD Lent: {Number(cdp.asset_lent / 10n ** BigInt(decimals))}</p>
-          <p>XLM Price: {lastpriceXLM}</p>
+          <p>USD Lent: {formatNumber(new BigNumber(cdp.asset_lent.toString()).div(10 ** decimals), decimals)}</p>
+          <p>XLM Price: {formatNumber(lastpriceXLM, 7)}</p>
           <p>
-            {symbolAsset} Price: {lastpriceAsset}
+            {symbolAsset} Price: {formatNumber(lastpriceAsset, 7)}
           </p>
         </>
       )}
