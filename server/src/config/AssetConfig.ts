7f54743f5bdbde821b52b6c4a7385852b9553756
Blaine Heffron <baheffron@gmail.com>
2025-01-28 14:53:14 -0500

    update retroshades table for updated deployed contract

--- server/src/config/AssetConfig.ts
+++ server/src/config/AssetConfig.ts
@@ -2,6 +2,7 @@ interface AssetDetails {
   feed_address: string;
   pool_address: string;
   retroshades_cdp: string;
+  retroshades_stake: string;
 }
 
 export interface AssetConfig {
@@ -13,6 +14,7 @@ export const assetConfig: AssetConfig = {
   'USDT': {
     feed_address: "CCYOZJCOPG34LLQQ7N24YXBM7LL62R7ONMZ3G6WZAAYPB5OYKOMJRN63",
     pool_address: "CCECE32QRX3PYIFAKH65ITCUT2JTYZTS4GEIFDWMQFSKJ5W6PVZWH2EE",
-    retroshades_cdp: 'cdp1a16c60a7890c14872ae7c3a025c31a9',
+    retroshades_cdp: 'cdpb40df2a82e55c35f5fe3abb8faa758fc',
+    retroshades_stake: 'stake_positionb40df2a82e55c35f5fe3abb8faa758fc',
   }
 }
