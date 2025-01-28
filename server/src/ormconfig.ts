d2906a138cdef818e95878e9732401920f7bc4db
Pam Selle <pam@ahalabs.dev>
2025-01-28 14:13:02 -0500

    Set DB name via env var for deploy

--- server/src/ormconfig.ts
+++ server/src/ormconfig.ts
@@ -12,11 +12,10 @@ const config: DataSourceOptions = {
   port: Number(process.env.DB_PORT) || 5432,
   username: process.env.DB_USER || "postgres",
   password: process.env.DB_PASSWORD || "postgres",
-  database: "equitxindexer",
+  database: process.env.DB_NAME || "equitxindexer",
   entities: [Asset, PriceHistory, CDP, Staker, LiquidityPool, ContractState ],
   synchronize: false,
   migrations: [__dirname + "/../migration/*.ts"],
-
 };
 
 export const AppDataSource = new DataSource(config);
