4dfc433704a0e05ee1844044a9d029054eb6f9cd
Pam Selle <pam@ahalabs.dev>
2025-04-14 13:12:06 -0400

    Update orchestrator

--- contracts/xasset-orchestrator/src/token.rs
+++ contracts/xasset-orchestrator/src/token.rs
@@ -1,8 +1,4 @@
-#![allow(unused)]
-use loam_sdk::{
-    import_contract,
-    soroban_sdk::{self, contractimport, xdr::ToXdr, Address, Bytes, BytesN, Env, IntoVal, String},
-};
+use loam_sdk::soroban_sdk::{xdr::ToXdr, Address, Bytes, BytesN, Env, String};
 
 use crate::error::Error;
 
@@ -22,11 +18,11 @@ pub fn create_contract(
     let mut salt = Bytes::new(e);
     salt.append(&asset_symbol.to_xdr(e));
     // owner is the admin of this orchestrator contract
-    let owner = e.current_contract_address()
+    let owner = e.current_contract_address();
     let salt = e.crypto().sha256(&salt);
     let address = e.deployer()
         .with_current_contract(salt)
-        .deploy_v2(token_wasm_hash.clone(), ())
+        .deploy_v2(token_wasm_hash.clone(), ());
     // Set the owner of the contract to this orchestrator
     let _ = xasset::Client::new(e, &address)
         .try_admin_set(&owner)
