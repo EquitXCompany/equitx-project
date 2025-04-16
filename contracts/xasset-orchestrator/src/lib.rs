6dfb2d69624c103dbe8580054050b486c64827da
Pam Selle <pam@ahalabs.dev>
2025-04-16 15:50:58 -0400

    Use import_contract! macro and remove data_feed

--- contracts/xasset-orchestrator/src/lib.rs
+++ contracts/xasset-orchestrator/src/lib.rs
@@ -9,14 +9,6 @@ use crate::error::Error;
 pub mod orchestrator;
 pub use orchestrator::*;
 
-pub mod data_feed {
-    use loam_sdk::soroban_sdk;
-
-    soroban_sdk::contractimport!(
-        file = "../../target/wasm32-unknown-unknown/release/data_feed.wasm"
-    );
-}
-
 #[derive_contract(Core(Admin), OrchestratorTrait(Storage))]
 pub struct Contract;
 
