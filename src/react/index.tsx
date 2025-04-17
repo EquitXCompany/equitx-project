3a290cf7b51ebf3366fee4a12c4a27f193436a2b
OjGrooms <32404879+OjGrooms@users.noreply.github.com>
2025-04-17 14:12:04 -0400

    feat: adds theme reactive branded chevron (#54)
    - additionally adds theme reactive drawer collapsed brand logo

--- src/react/index.tsx
+++ src/react/index.tsx
@@ -36,7 +36,7 @@ const router = createHashRouter([
             flexGrow: 1,
             height: "100vh",
             overflowY: "visible",
-            overflowX: "auto",            
+            overflowX: "auto",
           }}
         >
           <Outlet />
