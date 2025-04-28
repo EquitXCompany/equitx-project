31b4bf6ec5e20b3f44637e6071862790cdd93710
Pam Selle <pam@ahalabs.dev>
2025-04-28 14:26:47 -0400

    Add at risk logic

--- src/react/routes/portfolio.tsx
+++ src/react/routes/portfolio.tsx
@@ -4,6 +4,8 @@ import ErrorMessage from '../components/errorMessage';
 import { useContractCdpForAllAssets } from '../hooks/useCdps';
 import BigNumber from 'bignumber.js';
 import { useContractMapping } from '../../contexts/ContractMappingContext';
+import { useAssets } from '../hooks/useAssets';
+import { useAllStabilityPoolMetadata, useStabilityPoolMetadata } from '../hooks/useStabilityPoolMetadata';
 
 export default function Portfolio() {
     const { account } = useWallet();
@@ -16,6 +18,16 @@ export default function Portfolio() {
         enabled: !!account,
     });
 
+    const { data: spData, isLoading: spLoading } = useAllStabilityPoolMetadata(contractMapping);
+
+    function isCDPAtRisk(cdp: any, asset:any): boolean {
+        if (spLoading) return false;
+        const cdpRatio = cdp.collateralization_ratio;
+        const spRatio = spData[asset]?.min_ratio!;
+
+        const atRisk = Math.abs(cdpRatio - spRatio) <= 0.1 * spRatio;
+        return atRisk;
+    }
 
     return (<>
         {!account && (
@@ -37,22 +49,27 @@ export default function Portfolio() {
         ) : (
             <>
                 <Grid2 container columns={3} justifyContent='left' pb='10px' px='50px'>
-                {userCdpsMap && Object.entries(userCdpsMap).filter(([_, cdp]) => !!cdp).map(([asset, cdp]) => (
-                    <Paper key={asset} sx={{ p: 2, border: '1px solid', borderRadius: '4px', margin: 2, minWidth: '20em' }}>
-                        <Typography variant="h5">{asset}</Typography>
-                        <Typography>{cdp?.status.tag}</Typography>
-                        <Typography>{formatNumber(cdp?.asset_lent)} {asset} lent</Typography>
-                        <Typography>Collateralization Ratio: {BigNumber(cdp?.collateralization_ratio!/100).toFixed(2)}%</Typography>
-                        <Typography>{formatNumber(cdp?.xlm_deposited)} XLM Locked</Typography>
-                        <Typography>{formatNumber(cdp?.accrued_interest.amount)} interest accrued in {asset}</Typography>
-                        <Typography><Link
-                            href={`#/cdps/${asset}/${account}`}
-                            sx={{ textDecoration: 'none', color: 'primary.main' }}
-                        >
-                            View CDP
-                        </Link></Typography>
-                    </Paper>
-                ))}
+                    {userCdpsMap && Object.entries(userCdpsMap).filter(([_, cdp]) => !!cdp).map(([asset, cdp]) => {
+                        const atRisk = isCDPAtRisk(cdp, asset);
+                        return (
+                        <Paper key={asset} sx={{ p: 2, border: '1px solid', borderRadius: '4px', margin: 2, width: '25em', backgroundColor: atRisk ? '#D46565' : '' }}>
+                            <Typography variant="h5">{asset}</Typography>
+                            <Typography>{cdp?.status.tag}</Typography>
+                            {spData && atRisk && (
+                                <Typography variant="h6" color="white">CDP is at risk of liquidation! Minimum ratio for {asset} is {spData[asset]!.min_ratio/100}%</Typography>
+                            )}
+                            <Typography>{formatNumber(cdp?.asset_lent)} {asset} lent</Typography>
+                            <Typography>Collateralization Ratio: {BigNumber(cdp?.collateralization_ratio! / 100).toFixed(2)}%</Typography>
+                            <Typography>{formatNumber(cdp?.xlm_deposited)} XLM Locked</Typography>
+                            <Typography>{formatNumber(cdp?.accrued_interest.amount)} interest accrued in {asset}</Typography>
+                            <Typography><Link
+                                href={`#/cdps/${asset}/${account}`}
+                                sx={{ textDecoration: 'none', color: 'primary.main' }}
+                            >
+                                View CDP
+                            </Link></Typography>
+                        </Paper>
+                    )})}
                 </Grid2>
             </>
         )
@@ -60,7 +77,7 @@ export default function Portfolio() {
     </>);
 }
 
-function formatNumber(num :any) {
+function formatNumber(num: any) {
     const bN = new BigNumber(num);
-    return bN.div(10**7).toString();
+    return bN.div(10 ** 7).toString();
 }
