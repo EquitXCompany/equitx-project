6b4fb344576934754e6f8b1216b43b6feecc49de
Zach Fedor <zachfedor@gmail.com>
2025-12-09 12:36:01 -0500

    fix: optimize price history chart logic

--- src/react/routes/cdps/root.tsx
+++ src/react/routes/cdps/root.tsx
@@ -1,11 +1,10 @@
 import { Outlet, useNavigate, useParams } from "react-router-dom";
-import { Box, Typography, Grid, Paper, Container, Button } from '@mui/material';
-import Connect from "../../components/connect";
-import { useStabilityPoolMetadata } from '../../hooks/useStabilityPoolMetadata';
+import { Box, Typography, Grid, Paper, Button } from "@mui/material";
+import { useStabilityPoolMetadata } from "../../hooks/useStabilityPoolMetadata";
 import BigNumber from "bignumber.js";
 import { BASIS_POINTS } from "../../../constants";
 import ErrorMessage from "../../components/errorMessage";
-import PriceHistoryChart from '../../components/charts/PriceHistoryChart';
+import PriceHistoryChart from "../../components/charts/PriceHistoryChart";
 import { useLatestCdpMetrics } from "../../hooks/useCdpMetrics";
 import { formatCurrency } from "../../../utils/formatters";
 import { useLatestTVLMetrics } from "../../hooks/useTvlMetrics";
@@ -24,34 +23,53 @@ function Root() {
       />
     );
   }
-  const { data: assetMetrics, error: assetMetricsError, isLoading: assetMetricsLoading } = useLatestCdpMetrics(assetSymbol);
-  const { data: tvlMetrics, error: tvlMetricsError, isLoading: tvlMetricsLoading } = useLatestTVLMetrics(assetSymbol);
-  const { data: stabilityData, error, isLoading } = useStabilityPoolMetadata(assetSymbol, contractMapping);
+  const {
+    data: assetMetrics,
+    error: assetMetricsError,
+    isLoading: assetMetricsLoading,
+  } = useLatestCdpMetrics(assetSymbol);
+  const {
+    data: tvlMetrics,
+    error: tvlMetricsError,
+    isLoading: tvlMetricsLoading,
+  } = useLatestTVLMetrics(assetSymbol);
+  const {
+    data: stabilityData,
+    error,
+    isLoading,
+  } = useStabilityPoolMetadata(assetSymbol, contractMapping);
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>An error occurred: {error.message}</div>;
   if (!stabilityData) return <div>No data available</div>;
 
-  const { lastpriceXLM, lastpriceAsset, min_ratio, symbolAsset } = stabilityData;
+  const { lastpriceXLM, lastpriceAsset, min_ratio, symbolAsset } =
+    stabilityData;
 
   const handleStabilityPoolClick = () => {
     navigate(`/stability-pool/${assetSymbol}`);
   };
 
   return (
-    <Container maxWidth="lg">
+    <>
       <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          XLM↔{symbolAsset} Pool
-        </Typography>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleStabilityPoolClick}
-          sx={{ mt: 2, mb: 3 }}
-        >
-          View Stability Pool
-        </Button>
+        <Grid container sx={{ alignItems: "center", mb: 4 }}>
+          <Typography
+            variant="h5"
+            component="h2"
+            sx={{ textAlign: "left", m: 0 }}
+          >
+            XLM↔{symbolAsset} Pool
+          </Typography>
+          <Button
+            variant="outlined"
+            color="primary"
+            onClick={handleStabilityPoolClick}
+            sx={{ ml: 4 }}
+          >
+            View Stability Pool
+          </Button>
+        </Grid>
         <Grid container spacing={3}>
           <Grid item xs={true} sm={4}>
             <Paper elevation={3} sx={{ p: 2 }}>
@@ -59,7 +77,11 @@ function Root() {
                 Collateral Ratio
               </Typography>
               <Typography variant="h4">
-                {assetMetricsLoading ? "Loading..." : assetMetricsError ? "Error" : `${assetMetrics?.collateralRatio.toFixed(4)}%`}
+                {assetMetricsLoading
+                  ? "Loading..."
+                  : assetMetricsError
+                    ? "Error"
+                    : `${assetMetrics?.collateralRatio.toFixed(4)}%`}
               </Typography>
             </Paper>
           </Grid>
@@ -69,7 +91,11 @@ function Root() {
                 Market Cap USD
               </Typography>
               <Typography variant="h4">
-                {tvlMetricsLoading ? "Loading..." : tvlMetricsError ? "Error" : `${formatCurrency(tvlMetrics?.totalXassetsMinted.multipliedBy(lastpriceAsset) || '', 7, 2, "$")}`}
+                {tvlMetricsLoading
+                  ? "Loading..."
+                  : tvlMetricsError
+                    ? "Error"
+                    : `${formatCurrency(tvlMetrics?.totalXassetsMinted.multipliedBy(lastpriceAsset) || "", 7, 2, "$")}`}
               </Typography>
             </Paper>
           </Grid>
@@ -79,7 +105,11 @@ function Root() {
                 Market Cap XLM
               </Typography>
               <Typography variant="h4">
-                {tvlMetricsLoading ? "Loading..." : tvlMetricsError ? "Error" : `${formatCurrency(tvlMetrics?.totalXassetsMinted.multipliedBy(lastpriceAsset).dividedBy(lastpriceXLM) || '', 7, 2, "XLM")}`}
+                {tvlMetricsLoading
+                  ? "Loading..."
+                  : tvlMetricsError
+                    ? "Error"
+                    : `${formatCurrency(tvlMetrics?.totalXassetsMinted.multipliedBy(lastpriceAsset).dividedBy(lastpriceXLM) || "", 7, 2, "XLM")}`}
               </Typography>
             </Paper>
           </Grid>
@@ -89,7 +119,11 @@ function Root() {
                 Total Minted
               </Typography>
               <Typography variant="h4">
-                {tvlMetricsLoading ? "Loading..." : tvlMetricsError ? "Error" : `${formatCurrency(tvlMetrics?.totalXassetsMinted || '', 7, 2, assetSymbol)}`}
+                {tvlMetricsLoading
+                  ? "Loading..."
+                  : tvlMetricsError
+                    ? "Error"
+                    : `${formatCurrency(tvlMetrics?.totalXassetsMinted || "", 7, 2, assetSymbol)}`}
               </Typography>
             </Paper>
           </Grid>
@@ -98,9 +132,7 @@ function Root() {
               <Typography variant="h6" gutterBottom>
                 XLM Price
               </Typography>
-              <Typography variant="h4">
-                ${lastpriceXLM.toFixed(4)}
-              </Typography>
+              <Typography variant="h4">${lastpriceXLM.toFixed(4)}</Typography>
             </Paper>
           </Grid>
           <Grid item xs={12} sm={4}>
@@ -108,9 +140,7 @@ function Root() {
               <Typography variant="h6" gutterBottom>
                 {symbolAsset} Price
               </Typography>
-              <Typography variant="h4">
-                ${lastpriceAsset.toFixed(4)}
-              </Typography>
+              <Typography variant="h4">${lastpriceAsset.toFixed(4)}</Typography>
             </Paper>
           </Grid>
           <Grid item xs={12} sm={4}>
@@ -119,16 +149,20 @@ function Root() {
                 Minimum CR
               </Typography>
               <Typography variant="h4">
-                {new BigNumber(min_ratio).times(100).div(BASIS_POINTS).toFixed(2)}%
+                {new BigNumber(min_ratio)
+                  .times(100)
+                  .div(BASIS_POINTS)
+                  .toFixed(2)}
+                %
               </Typography>
             </Paper>
           </Grid>
         </Grid>
         <PriceHistoryChart assetSymbol={assetSymbol} />
       </Box>
-      <Connect />
+
       <Outlet />
-    </Container>
+    </>
   );
 }
 
